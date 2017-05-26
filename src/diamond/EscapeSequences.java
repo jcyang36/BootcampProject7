e8b4429f1c56bbcd3bf16c90ba1bd495b13d75f1
Joyce <jcyang@hmc.edu>
2017-05-26 09:54:23 -0400

    First commit

---
+++ src/diamond/EscapeSequences.java
@@ -0,0 +1,16 @@
+package diamond;
+public class EscapeSequences
+ {
+     public static void main( String[] args)
+    {
+
+       
+        System.out.println("*********\r\n");
+        System.out.println("*\t*\r\n");
+        System.out.println("*\t*\r\n");
+        System.out.println("*\t*\r\n");
+        System.out.println("*\t*\r\n");
+		System.out.println("*********\r");
+		
+     }
+ }
